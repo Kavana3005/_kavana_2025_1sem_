abe28095fa922d39e302286269e3524e811fe317
SUHANA NAAZ <suhananaaz59@gmail.com>
2025-11-20 09:41:47 +0530

    day4 code push

---
+++ day4/2_secondprg.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int  main() int a, b, product;
+{
+    prinf("enter the first value a");
+    scanf("%d", &a);
+    pprintf("enter the value of b");
+    scanf("%d", &b);
+    product = a * b;
+    printf("product of %d and %d;%d\n", a, b, product);
+}
---
+++ day4/3_variable.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<stdbool.h>
+int main(){
+    int age=19;
+    printf("your age is :%d",age);
+     float marks = 345.67;
+    printf("\nyour marks is :%f",marks);
+    double  pi=3.148;
+    printf("\npi value is   :%lf",pi);
+    char gender = "f";
+    printf("\n your\? gender is :%c",gender);
+    bool iswalking = true;
+    printf("\n is\"walking\":%d",iswalking);
+}
